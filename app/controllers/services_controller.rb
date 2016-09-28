81a3214e3cb787c5ac868b0c308d6ddbfb929473
softwarenacho <trsgarp@gmail.com>
2016-09-28 11:40:17 -0500

    puts

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,6 @@
 class ServicesController < ApplicationController
   def send_zoho
+    puts "WTF"*100
     base_request = "https://crm.zoho.com/crm/private/json/Leads/insertRecords?authtoken=#{ENV['ZOHO_TOKEN']}&scope=crmapi&xmlData="
     changes = ""
     changes += "<FL val='Last Name'>#{params["name"]}</FL>"
@@ -13,10 +14,20 @@ class ServicesController < ApplicationController
     time = Time.now.strftime("%m/%d/%Y %H:%M:%S")
     changes += "<FL val='Created at'>#{time}</FL>"
     base_xmldata = "<Leads><row no='1'>#{changes}</row></Leads>"
+    puts "Request"
+    puts base_request + base_xmldata
     request = URI.parse(URI.escape(base_request + base_xmldata))
+    puts "request"
+    p request
     check = JSON.parse(Net::HTTP.get(request))
+    puts "check"
+    p check
     zoho_id = check["response"]["result"]["recorddetail"]["FL"].first["content"]
+    puts "Zoho id"
+    p zoho_id
     hash = {id: zoho_id , response: {result: true, params: params}}
+    puts "Hash"
+    p hash
     render json: hash.to_json
   end
 end
