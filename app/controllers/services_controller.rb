59130ccb64d80bb4f6e68cc516e58b882d0293cc
softwarenacho <trsgarp@gmail.com>
2016-09-28 09:40:06 -0500

    Corrections in render JSON

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,6 @@
 class ServicesController < ApplicationController
   def send_zoho
     p params
-    render json: JSON.parse({response: {result: true}})
+    render json: {response: {result: true, params: params}}.to_json
   end
 end
